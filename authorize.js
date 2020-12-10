bb7c930f2a76e4453bad894c629198a0f67576be
theonlyamos <theonlyamos@gmail.com>
2020-12-10 19:33:37 +0000

    admin authorization

--- authorize.js
+++ authorize.js
@@ -1,10 +1,11 @@
 const isAdmin = function(req, res, next){
+    console.log(req.user)
     if (req.user.role == 'administrator'){
         next()
     }else {
       req.session.error = true
       req.session.errMsg = "Not authorized"
-      req.session.destroy();
+      //req.session.destroy();
       res.clearCookie();
       res.redirect('/login')
     }
